dfa23904a74a70da196f8457311eb0959beb7683
Kazuki Negoro <negokaz@gmail.com>
2023-06-18 20:27:36 +0900

    各種ファイル作成機能

--- core/src/extension.ts
+++ core/src/extension.ts
@@ -1,9 +1,12 @@
 import * as vscode from 'vscode';
 import { ConsoleViewManager } from './console/consoleViewManager';
+import { Messages } from '@ui/i18n/messages';
 
 export function activate(context: vscode.ExtensionContext) {
 
     const consoleViewManager = new ConsoleViewManager(context);
+    
+    const messages = new Messages(vscode.env.language);
 
 	console.log('Congratulations, your extension "markdown-console" is now active!');
 
@@ -27,8 +30,63 @@ export function activate(context: vscode.ExtensionContext) {
                 await consoleViewManager.openPreview(document);
             }
 		}),
+        vscode.commands.registerCommand('markdown-console.generateConMd', async (uri: vscode.Uri) => {
+            const input = await vscode.window.showInputBox({ prompt: messages.get('input-new-con-md-file-name') });
+            if (input) {
+                const fileName = input.length > 0 ? input : 'newFile';
+                const mdUri = uri.with({ path: uri.path + `/${fileName}.con.md` });
+                const edit = new vscode.WorkspaceEdit();
+                edit.createFile(mdUri, { ignoreIfExists: true, contents: Buffer.from(ConMdTemplate(fileName)) });
+                await vscode.workspace.applyEdit(edit);
+                const document = await vscode.workspace.openTextDocument(mdUri);
+                await vscode.window.showTextDocument(document);
+                await consoleViewManager.openPreview(document);
+            }
+        }),
+        vscode.commands.registerCommand('markdown-console.generateMarkdownConsoleYml', async (uri: vscode.Uri) => {
+            const ymlUri = uri.with({ path: uri.path + '/markdown-console.yml' });
+            const edit = new vscode.WorkspaceEdit();
+            edit.createFile(ymlUri, { ignoreIfExists: true, contents: Buffer.from(MarkdownConsoleYmlTemplate) });
+            await vscode.workspace.applyEdit(edit);
+            const document = await vscode.workspace.openTextDocument(ymlUri);
+            await vscode.window.showTextDocument(document);
+        }),
 	);
 }
+
+const ConMdTemplate = (title: string) => `# ${title}
+
+\`\`\`bash
+#@cmd:[bash -c]
+echo Hello, World!
+\`\`\`
+`;
+
+const MarkdownConsoleYmlTemplate = `# Markdown Console Configuration File
+# You can override each value by creating a file which was named "markdown-console_*.yml".
+# (e.g., markdown-console_override.yml, markdown-console_prod.yml)
+
+# Environment variables to run commands
+# 
+# [example]
+# env:
+#    PATH:
+#        - "C:/Program Files/Git/bin;"
+#        - "{{PATH}}"
+env: 
+    PATH:
+        - "{{PATH}}"
+
+# Markdown Console (*.con.md) templating variables
+# 
+# [example]
+# variable:
+#    server1:
+#        user: "user1"
+#        host: "server1.example.com"
+variable:
+    var1: example
+`;
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
